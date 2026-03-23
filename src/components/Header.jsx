079a9e0781672ee035643623273c8a33d6b898b5
Zion <226828312+kxlade@users.noreply.github.com>
2026-03-23 21:07:48 +0100

    fixed a lil typo in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,27 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartArrowDown } from "@fortawesome/free-solid-svg-icons";
-import { faArrowCircleDown } from "@fortawesome/free-solid-svg-icons";
-import './Header.css';
-const Header = ({ setPage }) => {
-
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCartArrowDown} from "@fortawesome/free-solid-svg-icons";
+import {faArrowCircleDown} from "@fortawesome/free-solid-svg-icons";
+import "./Header.css";
+const Header = ({setPage}) => {
   // const coffee = "\u2615";
   // const blanket = "\uD83C\uDF81";
   // const plant = "\uD83C\uDF3F";
-
-
 
   return (
     <header className="header-cont">
       <div className="header_inner">
         <h1 className="header_title">
-          Welcome to{" "}
-          <span className="header_title-highlight">Shopping<span className="navbar_logo-dot">cart</span></span>{" "}
-          <FontAwesomeIcon icon={faCartArrowDown} fade/>
+          Welcome to <span className="header_title-highlight">
+            Shopping<span className="navbar_logo-dot">cart</span>
+          </span>
+          <FontAwesomeIcon icon={faCartArrowDown} fade />
         </h1>
 
         <p className="header_subtitle">Your one-stop shop for all your needs</p>
 
         <button className="header_button" onClick={() => setPage("shop")}>
-          Shop Now <FontAwesomeIcon icon={faArrowCircleDown} fade/>
+          Shop Now <FontAwesomeIcon icon={faArrowCircleDown} fade />
         </button>
-
       </div>
     </header>
   );
